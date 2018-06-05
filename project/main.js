051272cf5b4f1b4af20c628f8e632e198df38e80
Hannah Hudson <hhudson@riversagency.com>
2018-06-04 22:50:18 -0400

    done?

---
+++ project/main.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+  document.getElementById('loader').className += ' loader--hidden';
+}, 2000); 
+
+
+window.onload = function() {
+    document.getElementById('loader').className += ' loader--hidden';
+}; 
